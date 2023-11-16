9e22794af993df4016f9529625f5c2c3b981bfb6
bruno.mairey <bruno.mairey@openforce.com>
2023-11-16 09:57:26 +0100

    feat: first simple implementation (#main)

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React, {FunctionComponent, useEffect, useState} from 'react';
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/mock-pokemon";
+
+const App: FunctionComponent = () => {
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+
+    useEffect(() => {
+       setPokemons(POKEMONS)
+    }, []);
+
+    return (
+        <div>
+            <h1 className="center">Pokemon App</h1>
+            <div className="container">
+                <div className="row">
+                    {pokemons.map(({id,name, picture,created }) => (
+                        <div className="col s6 m4" key={id}>
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img src={picture} alt={name}/>
+                                </div>
+                                <div className="card-stacked">
+                                    <div className="card-content">
+                                        <p>{name}</p>
+                                        <p><small>{created.toString()}</small></p>
+                                    </div>
+                                    {/*<div className="card-action">*/}
+                                    {/*    <a href="#">Détails</a>*/}
+                                    {/*</div>*/}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+           </div>
+        </div>
+
+    )
+}
+
+export default App;
+
+// ComponentDidMount
+// componentDidUpdate
+// componentWillUnmount
